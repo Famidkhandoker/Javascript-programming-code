078ff9fecac241c53f1f14e94d1bc9ffa126bbab
FAMID KHANDOKER <famidkhandoker@gmail.com>
2025-05-27 21:09:19 +0600

    Add files via upload

---
+++ Javascript_practise/Fetch/task4.js
@@ -0,0 +1,16 @@
+const data = {
+    userId: 7,
+    action: "login",
+    timestamp: 1690001234567,
+  };
+  
+  // JSON ফরম্যাটে রূপান্তর করা
+  const dataJSON = JSON.stringify(data);
+  console.log(dataJSON);
+  
+  // JSON স্ট্রিং এর টাইপ দেখা
+  console.log(typeof dataJSON);
+
+  //output
+  //{"userId":7,"action":"login","timestamp":1690001234567}
+  //string
